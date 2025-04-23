7821b37a076ebe791adb8a3a1bf8b9c4827a98a2
hajji31 <ahajji002@ikasle.ehu.eus>
2025-04-23 02:17:10 +0200

    google analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import ReactGA from 'react-ga4';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import About from './components/About';
@@ -9,11 +10,17 @@ import './App.css';
 const App = () => {
   const [language, setLanguage] = useState('fr'); // Default language set to French
 
+  useEffect(() => {
+    // Initialize Google Analytics with your Measurement ID
+    ReactGA.initialize('G-0VWKBBS253'); // Replace with your actual Measurement ID
+    ReactGA.send('pageview'); // Track the initial page view
+  }, []);
+
   return (
     <>
       <Navbar language={language} setLanguage={setLanguage} />
       <Hero language={language} />
-      <About language={language}/>
+      <About language={language} />
       <Education language={language} />
       <Projects language={language} />
     </>
